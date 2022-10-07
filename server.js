08820a4bab4d5c74977dae87c72402e25aaf358f
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2022-10-06 21:43:23 -0300

    Instalando o CORS

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 let fs = require("fs");
+let cors = require('cors');
+const { Server } = require('http');
 let wordlist = fs.readFileSync("./br-utf8.txt", "UTF-8").split("\n")
+server.use(cors())
 let gerarPalavra = () => {
     return wordlist[Math.floor(Math.random() * wordlist.length)]
 }
@@ -16,8 +19,6 @@ app.get('/', (req,res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
-
-fetch
 // const { get } = require('http')
 
 // let palavras = fs.readFileSync("./br-utf8.txt", "UTF-8").split("\n")
