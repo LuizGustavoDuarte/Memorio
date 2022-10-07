277b862ee8ac3c678a692a0d417b60e88de321cd
LuizGustavoDuarte <elfzero0@gmail.com>
2022-10-06 21:30:10 -0300

    adicionando exemplo

--- main.js
+++ main.js
@@ -4,48 +4,15 @@ let passou = document.querySelectorAll("button")[0]
 let nova = document.querySelectorAll("button")[1]
 let score = 0
 
-let wordlist = [
-"Aarão",
-"Abel",
-"Abelardo",
-"Abissínia",
-"Abner",
-"Abraão",
-"Absalão",
-"Acab",
-"Acaia",
-"Acker",
-"Aconcágua",
-"Acre",
-"Acrópole",
-"Adauto",
-"Ademir",
-"Adhemar",
-"Adolfo",
-"Adriana",
-"Adriano",
-"Adriático",
-"Adroaldo",
-"Afeganistão",
-"Afonso",
-"Afrodite",
-"Agostinho",
-"Aires",
-"Ajax",
-"Alagoas",
-"Alair",
-"Alarico",
-"Alasca",
-"Alberta",
-"Albertina",
-"Alberto",
-"Alceu"
-]
-
 let passadas = []
 
 let gerarPalavra = () => {
-    palavra.innerHTML = wordlist[Math.floor(Math.random() * wordlist.length)]
+
+    let resp = ""
+    fetch("http://localhost:3000", {method: "GET"}).then(resposta => {resp = resposta}).then(() => {
+        console.log(resp)
+        palavra.innerHTML = resp
+    })
 }
 
 passou.addEventListener("click", () => {
