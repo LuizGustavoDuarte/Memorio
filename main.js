c14388586c42c11ed3a98ef08a41a6cbce6568a9
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2022-10-06 21:29:34 -0300

    Adicionando o server

--- main.js
+++ main.js
@@ -1,46 +1,7 @@
 let palavra = document.querySelector("h3")
-
 let passou = document.querySelectorAll("button")[0]
 let nova = document.querySelectorAll("button")[1]
 let score = 0
-
-let wordlist = [
-"Aarão",
-"Abel",
-"Abelardo",
-"Abissínia",
-"Abner",
-"Abraão",
-"Absalão",
-"Acab",
-"Acaia",
-"Acker",
-"Aconcágua",
-"Acre",
-"Acrópole",
-"Adauto",
-"Ademir",
-"Adhemar",
-"Adolfo",
-"Adriana",
-"Adriano",
-"Adriático",
-"Adroaldo",
-"Afeganistão",
-"Afonso",
-"Afrodite",
-"Agostinho",
-"Aires",
-"Ajax",
-"Alagoas",
-"Alair",
-"Alarico",
-"Alasca",
-"Alberta",
-"Albertina",
-"Alberto",
-"Alceu"
-]
 
 let passadas = []
 
--- server.js
+++ server.js
@@ -1,7 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
 let fs = require("fs");
+let wordlist = fs.readFileSync("./br-utf8.txt", "UTF-8").split("\n")
+let gerarPalavra = () => {
+    return wordlist[Math.floor(Math.random() * wordlist.length)]
+}
 
-let palavras = fs.readFileSync("./br-utf8.txt", "UTF-8").split("\n")
+app.get('/', (req,res) => {
+    res.send(gerarPalavra())
+})
 
-console.log(palavras)
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+fetch
+// const { get } = require('http')
+
+// let palavras = fs.readFileSync("./br-utf8.txt", "UTF-8").split("\n")
+
+// console.log(palavras)
 
 
