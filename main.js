b6c63f7f41d178b1fdcacf36f2f1ec07a8e28fde
LuizGustavoDuarte <elfzero0@gmail.com>
2022-10-10 14:49:42 -0300

    Pontos e funcionamento total definitivo (pre-css-update)

--- main.js
+++ main.js
@@ -1,40 +1,51 @@
 let palavra = document.querySelector("h3")
 let passou = document.querySelectorAll("button")[0]
 let nova = document.querySelectorAll("button")[1]
+let pontos = document.querySelector("#pontos")
 let score = 0
-
+let errado = false
 let passadas = []
 
 let gerarPalavra = () => {
-    if( (Math.floor(Math.random() * 10) ) <= 2 && passadas.length > 1) {
+    if ((Math.floor(Math.random() * 10)) <= 3 && passadas.length > 1) {
         palavra.innerHTML = passadas[Math.floor(Math.random() * passadas.length)]
     } else {
         let resp = ""
-        fetch("http://localhost:3000", {method: "GET"}).then(data => data.json()).then(resposta => {resp = resposta.palavra}).then(() => {
+        fetch("http://localhost:3000", { method: "GET" }).then(data => data.json()).then(resposta => { resp = resposta.palavra }).then(() => {
             console.log(resp)
             palavra.innerHTML = resp
         })
     }
 }
 
-passou.addEventListener("click", () => {
-    if(passadas.includes(palavra.innerHTML)) {
-        passadas.push(palavra.innerHTML)
+let derrota = () => {
+    
+    palavra.innerHTML = "Tente Novamente"
+    Swal.fire({
+        title: 'Perdeu!',
+        text: 'Tente Novamente',
+        icon: 'error',
+        confirmButtonText: 'Recom'
+    }).then(() => {
+        score = 0
+        pontos.innerHTML = score
         gerarPalavra()
-        console.log(passadas)
-        score++;
-    } else {
-        window.alert("Perdeu")
-    }
-})
+        passadas = []
+    })
 
-nova.addEventListener("click", () => {
-    if(passadas.includes(palavra.innerHTML)) {
-        window.alert("Perdeu")
-    } else {
-        passadas.push(palavra.innerHTML)
-        gerarPalavra()
-        console.log(passadas)
-        score++;
-    }
-})
+    
+}
+
+let acertou = ()  =>  {
+    passadas.push(palavra.innerHTML)
+    gerarPalavra()
+    console.log(passadas)
+    score++;
+    pontos.innerHTML = score
+}
+
+gerarPalavra()
+
+passou.addEventListener( "click", () => {(passadas.includes(palavra.innerHTML)) ? acertou() : derrota()} )
+
+nova.addEventListener( "click", () => {(passadas.includes(palavra.innerHTML)) ? derrota() : acertou()} )
--- server.js
+++ server.js
@@ -1,27 +1,25 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
 let fs = require("fs");
 let cors = require('cors');
-let wordlist = fs.readFileSync("./br-utf8.txt", "UTF-8").split("\n")
 app.use(cors())
+
+
+let wordlist = fs.readFileSync("./br-utf8.txt", "UTF-8").split("\n")
+
+
 let gerarPalavra = () => {
     return wordlist[Math.floor(Math.random() * wordlist.length)]
 }
 
 app.get('/', (req,res) => {
-    res.send(gerarPalavra())
+    res.send(JSON.stringify({palavra: gerarPalavra().toUpperCase()}))
 })
-
-
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
-// const { get } = require('http')
-
-// let palavras = fs.readFileSync("./br-utf8.txt", "UTF-8").split("\n")
-
-// console.log(palavras)
 
 
